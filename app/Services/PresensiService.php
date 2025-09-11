db192c6248adb3e646ac9b861b5b49c43cafec85
Yahya Zulfikri <zulfikriyahya18@gmail.com>
2025-09-12 04:00:56 +0700

    Updates Dispatch Whatsapp

--- app/Services/PresensiService.php
+++ app/Services/PresensiService.php
@@ -11,7 +11,6 @@ use App\Models\PresensiSiswa;
 use App\Models\JadwalPresensi;
 use App\Models\PresensiPegawai;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Cache;
 use App\Jobs\SendWhatsappNotification;
 
