cc0c6d3019d3a92147f8ce6858586f9ca0a372f5
Yahya Zulfikri <zulfikriyahya18@gmail.com>
2025-09-13 03:46:59 +0700

    Adding Widget

--- app/Filament/Resources/InformasiResource/Pages/ListInformasis.php
+++ app/Filament/Resources/InformasiResource/Pages/ListInformasis.php
@@ -5,13 +5,23 @@ namespace App\Filament\Resources\InformasiResource\Pages;
 use Filament\Actions;
 use Filament\Actions\CreateAction;
 use Filament\Support\Colors\Color;
+use Illuminate\Support\Facades\Auth;
 use Filament\Resources\Pages\ListRecords;
 use App\Filament\Resources\InformasiResource;
+use App\Filament\Resources\InformasiResource\Widgets\StatsOverview;
 
 class ListInformasis extends ListRecords
 {
     protected static string $resource = InformasiResource::class;
-
+    protected function getHeaderWidgets(): array
+    {
+        if (Auth::user()->hasRole('super_admin')) {
+            return [
+                StatsOverview::class,
+            ];
+        }
+        return [];
+    }
     protected function getHeaderActions(): array
     {
         return [
---
+++ app/Filament/Resources/InformasiResource/Widgets/StatsOverview.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Filament\Resources\InformasiResource\Widgets;
+
+use App\Models\Informasi;
+use Filament\Support\Colors\Color;
+use Filament\Widgets\StatsOverviewWidget\Stat;
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+
+class StatsOverview extends BaseWidget
+{
+    protected static bool $isLazy = false;
+
+    protected function getStats(): array
+    {
+        return [
+            Stat::make('Status Draft', Informasi::where('status', 'Draft')->count())
+                ->chartColor(Color::Orange)
+                ->chart([7, 2, 10, 3, 15, 4, 10]),
+            Stat::make('Status Publish', Informasi::where('status', 'Publish')->count())
+                ->chartColor(Color::Cyan)
+                ->chart([10, 2, 7, 3, 15, 4, 17]),
+            Stat::make('Status Archive', Informasi::where('status', 'Archive')->count())
+                ->chartColor(Color::Zinc)
+                ->chart([7, 2, 10, 3, 15, 4, 10]),
+        ];
+    }
+}
