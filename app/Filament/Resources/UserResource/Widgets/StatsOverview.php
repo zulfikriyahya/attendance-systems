faaa4add7fcfdcec0d696f1a46136a1b337b4f49
Yahya Zulfikri <zulfikriyahya18@gmail.com>
2025-09-13 03:41:33 +0700

    Adding Widget

--- app/Filament/Resources/UserResource/Widgets/StatsOverview.php
+++ app/Filament/Resources/UserResource/Widgets/StatsOverview.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Resources\UserResource\Widgets;
 
 use App\Models\User;
+use Blueprint\Models\Model;
 use Filament\Support\Colors\Color;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
