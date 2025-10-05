66ab7be04edcced2f5a9eafdecc3051d263728ab
Yahya Zulfikri <zulfikriyahya18@gmail.com>
2025-10-05 16:55:17 +0700

    Adding Feature: Mata Pelajaran.

--- database/migrations/2025_10_05_133824_create_guru_mata_pelajarans_table.php
+++ database/migrations/2025_10_05_133824_create_guru_mata_pelajarans_table.php
@@ -13,24 +13,20 @@ return new class extends Migration
     {
         Schema::create('guru_mata_pelajarans', function (Blueprint $table) {
             $table->uuid('id')->primary(); // Primary key berbentuk UUID
-
             // Relasi ke tabel kelas tahun pelajaran
             $table->foreignUuid('mata_pelajaran_id')
                 ->constrained('mata_pelajarans')
                 ->cascadeOnDelete()
                 ->cascadeOnUpdate()
                 ->nullable();
-
             // Relasi ke tabel pegawai
             $table->foreignUuid('pegawai_id')
                 ->constrained('pegawais')
                 ->cascadeOnDelete()
                 ->cascadeOnUpdate()
                 ->nullable();
-
             $table->timestamps();   // created_at & updated_at
             $table->softDeletes();  // deleted_at untuk soft delete
-
             // Unik kombinasi (mata_pelajarans, pegawais) agar tidak ada duplikasi
             $table->unique(['mata_pelajaran_id', 'pegawai_id'], 'guru_mata_pelajaran_unique');
         });
