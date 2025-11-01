7731ef685a4a05291677e0a991c6d624e1a74e30
Yahya Zulfikri <zulfikriyahya18@gmail.com>
2025-11-01 20:22:54 +0700

    Updating Tahun Pelajaran Resource

--- app/Filament/Resources/TahunPelajaranResource.php
+++ app/Filament/Resources/TahunPelajaranResource.php
@@ -2,38 +2,38 @@
 
 namespace App\Filament\Resources;
 
-use App\Filament\Resources\TahunPelajaranResource\Pages\CreateTahunPelajaran;
-use App\Filament\Resources\TahunPelajaranResource\Pages\EditTahunPelajaran;
-use App\Filament\Resources\TahunPelajaranResource\Pages\ListTahunPelajarans;
-use App\Filament\Resources\TahunPelajaranResource\Pages\ViewTahunPelajaran;
-use App\Models\TahunPelajaran;
 use Carbon\Carbon;
-use Filament\Forms\Components\DatePicker;
-use Filament\Forms\Components\Select;
-use Filament\Forms\Components\Textarea;
-use Filament\Forms\Components\TextInput;
-use Filament\Forms\Components\Toggle;
 use Filament\Forms\Form;
+use Filament\Tables\Table;
+use App\Models\TahunPelajaran;
 use Filament\Resources\Resource;
 use Filament\Support\Colors\Color;
-use Filament\Tables\Actions\ActionGroup;
-use Filament\Tables\Actions\DeleteAction;
-use Filament\Tables\Actions\DeleteBulkAction;
+use Illuminate\Support\Facades\Auth;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Textarea;
 use Filament\Tables\Actions\EditAction;
-use Filament\Tables\Actions\ForceDeleteAction;
-use Filament\Tables\Actions\ForceDeleteBulkAction;
-use Filament\Tables\Actions\RestoreAction;
-use Filament\Tables\Actions\RestoreBulkAction;
 use Filament\Tables\Actions\ViewAction;
-use Filament\Tables\Columns\ImageColumn;
 use Filament\Tables\Columns\TextColumn;
+use Filament\Forms\Components\TextInput;
+use Filament\Tables\Actions\ActionGroup;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Forms\Components\DatePicker;
+use Filament\Tables\Actions\DeleteAction;
 use Filament\Tables\Columns\ToggleColumn;
+use Illuminate\Database\Eloquent\Builder;
+use Filament\Tables\Actions\RestoreAction;
 use Filament\Tables\Enums\ActionsPosition;
 use Filament\Tables\Filters\TrashedFilter;
-use Filament\Tables\Table;
-use Illuminate\Database\Eloquent\Builder;
+use Filament\Tables\Actions\DeleteBulkAction;
+use Filament\Tables\Actions\ForceDeleteAction;
+use Filament\Tables\Actions\RestoreBulkAction;
+use Filament\Tables\Actions\ForceDeleteBulkAction;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
-use Illuminate\Support\Facades\Auth;
+use App\Filament\Resources\TahunPelajaranResource\Pages\EditTahunPelajaran;
+use App\Filament\Resources\TahunPelajaranResource\Pages\ViewTahunPelajaran;
+use App\Filament\Resources\TahunPelajaranResource\Pages\ListTahunPelajarans;
+use App\Filament\Resources\TahunPelajaranResource\Pages\CreateTahunPelajaran;
 
 class TahunPelajaranResource extends Resource
 {
@@ -57,53 +57,64 @@ class TahunPelajaranResource extends Resource
     {
         return $form
             ->schema([
-                Select::make('instansi_id')
-                    ->relationship('instansi', 'nama')
-                    ->required(),
-                TextInput::make('nama')
-                    ->label('Tahun Pelajaran')
-                    ->placeholder('Contoh: 2025/2026')
-                    ->minLength(9)
-                    ->maxLength(9)
-                    ->unique(
-                        ignorable: fn ($record) => $record,
-                        table: TahunPelajaran::class,
-                        column: 'nama',
-                    )
-                    ->required()
-                    ->validationMessages([
-                        'min' => 'Tahun Pelajaran tidak boleh kurang dari 9 karakter.',
-                        'max' => 'Tahun Pelajaran tidak boleh lebih dari 9 karakter.',
-                        'required' => 'Form ini harus diisi.',
-                        'unique' => 'Tahun Pelajaran sudah ada.',
-                    ]),
-
-                DatePicker::make('mulai')
-                    ->label('Tanggal Mulai')
-                    ->required()
-                    ->validationMessages([
-                        'required' => 'Form ini harus diisi.',
+                Section::make('Tahun Pelajaran')
+                    ->collapsible()
+                    ->columns([
+                        'sm' => 1,
+                        'md' => 3,
+                        'xl' => 5,
                     ])
-                    ->default(Carbon::create(now()->year, 7, 1)),
-                DatePicker::make('selesai')
-                    ->label('Tanggal Selesai')
-                    ->required()
-                    ->validationMessages([
-                        'required' => 'Form ini harus diisi.',
-                    ])
-                    ->default(Carbon::create(now()->year + 1, 6, 30)),
-                Textarea::make('deskripsi')
-                    ->label('Deskripsi')
-                    ->columnSpanFull(),
+                    ->schema([
+                        Select::make('instansi_id')
+                            ->relationship('instansi', 'nama')
+                            ->required(),
+                        TextInput::make('nama')
+                            ->label('Tahun Pelajaran')
+                            ->placeholder('Contoh: 2025/2026')
+                            ->minLength(9)
+                            ->maxLength(9)
+                            ->unique(
+                                ignorable: fn ($record) => $record,
+                                table: TahunPelajaran::class,
+                                column: 'nama',
+                            )
+                            ->required()
+                            ->validationMessages([
+                                'min' => 'Tahun Pelajaran tidak boleh kurang dari 9 karakter.',
+                                'max' => 'Tahun Pelajaran tidak boleh lebih dari 9 karakter.',
+                                'required' => 'Form ini harus diisi.',
+                                'unique' => 'Tahun Pelajaran sudah ada.',
+                            ]),
 
-                Toggle::make('status')
-                    ->label('Status')
-                    ->default(true)
-                    ->required()
-                    ->validationMessages([
-                        'required' => 'Form ini harus diisi.',
+                        DatePicker::make('mulai')
+                            ->label('Tanggal Mulai')
+                            ->required()
+                            ->validationMessages([
+                                'required' => 'Form ini harus diisi.',
+                            ])
+                            ->default(Carbon::create(now()->year, 7, 1)),
+                        DatePicker::make('selesai')
+                            ->label('Tanggal Selesai')
+                            ->required()
+                            ->validationMessages([
+                                'required' => 'Form ini harus diisi.',
+                            ])
+                            ->default(Carbon::create(now()->year + 1, 6, 30)),
+                        Select::make('status')
+                            ->default(true)
+                            ->native(false)
+                            ->options([
+                                true => 'Aktif',
+                                false => 'Non Aktif',
+                            ])
+                            ->required()
+                            ->validationMessages([
+                                'required' => 'Form ini wajib diisi.',
+                            ]),
+                        Textarea::make('deskripsi')
+                            ->label('Deskripsi')
+                            ->columnSpanFull(),
                     ]),
-
             ]);
     }
 
